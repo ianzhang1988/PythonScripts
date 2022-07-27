dd0db44448eb75c5aa5aba557b50716708185a4c
Ian.Zhang <ian.zhang.88@outlook.com>
2022-07-27 17:42:00 +0800

    Update hdfs_up_down.py

--- network/hdfs/hdfs_up_down.py
+++ network/hdfs/hdfs_up_down.py
@@ -7,17 +7,17 @@ from hdfs import Config
 from hdfs import InsecureClient
 
 #Init
-myClient = InsecureClient('http://snn1.heracles.sohuno.com:50070;http://snn2.heracles.sohuno.com:50070', 'sohuvideo', root='/user/sohuvideo')
+myClient = InsecureClient('http://127.0.0.1:50070;http://127.0.0.150070', 'video', root='/user/video')
 
 #List HDFS file
-print(myClient.list('/user/sohuvideo/'))
+print(myClient.list('/user/video/'))
 
-#Upload file to HDFS, localsource=/tmp/gmond.log, HDFSdest=/user/sohuvideo/
-myClient.upload('/user/sohuvideo/test/', 'd:/Temp/star.war.4.270s.mp4')
+#Upload file to HDFS, localsource=/tmp/gmond.log, HDFSdest=/user/video/
+myClient.upload('/user/video/test/', 'd:/Temp/star.war.4.270s.mp4')
 
 #Download file from HDFS, source=/user/sohuvideo/testfile, localpath=tmp
-myClient.download('/user/sohuvideo/test/star.war.4.270s.mp4', './')
+myClient.download('/user/video/test/star.war.4.270s.mp4', './')
 
 
-print(myClient.list('/user/sohuvideo/test'))
+print(myClient.list('/user/video/test'))
 
